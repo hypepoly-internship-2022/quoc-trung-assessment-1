5e4e4b23fbc81ff7da636593bf616ee4521ce2b2
quoctrungvu1610 <80931191+quoctrungvu1610@users.noreply.github.com>
2022-11-21 15:15:10 +0700

    +add Obstacle, Obstacle Material and PlayerCollision script

---
+++ assessment-1/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle")
+        {
+            playerMovement.enabled = false;
+        }
+    }
+}
